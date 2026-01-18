ff76f3b7ed738063615be98ca54749f9a18e74ed
Lapcuatui <macone-cw38zm@MacOne-CW38ZMs-MacBook-Pro.local>
2026-01-18 11:33:37 +0700

    fix systax BASE_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const instance = axios.create({ 
-  baseURL: "BASE_URL",
+  baseURL: BASE_URL,
 })
 
 export default instance
