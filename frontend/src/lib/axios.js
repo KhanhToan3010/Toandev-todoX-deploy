8955ed9c024107d1acadc6add04811863bd67f31
Lapcuatui <macone-cw38zm@MacOne-CW38ZMs-MacBook-Pro.local>
2026-01-18 10:58:41 +0700

    init deploy render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:5001/api",
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const instance = axios.create({ 
+  baseURL: "BASE_URL",
 })
 
 export default instance
